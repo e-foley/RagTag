e7f635ae7ed8392458fcac309c40d50c3a45813b
Ed Foley <ed.l.foley@gmail.com>
2024-11-18 15:49:59 -0800

    add test for TagMap::getAllTags()

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -56,3 +56,34 @@ TEST_CASE("TagMap tag re-registration with new value", "[all][TagMap-2]") {
   REQUIRE(tag_ret.has_value());
   CHECK(*tag_ret == "Tag Z");
 }
+
+TEST_CASE("TagMap getAllTags()", "[all][TagMap-3]") {
+  TagMap map;
+  CHECK(map.getAllTags().size() == 0);
+
+  // Intentionally add tag numbers out of order to ensure later that getAllTags() sorts by value.
+  REQUIRE(map.addTag(300, "#300"));
+  REQUIRE(map.addTag(100, "#100"));
+  REQUIRE(map.addTag(400, "#400"));
+  REQUIRE(map.addTag(500, "#500"));
+  REQUIRE(map.addTag(200, "#200"));
+
+  auto all_tags = map.getAllTags();
+  REQUIRE(all_tags.size() == 5);
+
+  // Confirm that the tags are sorted numerically by ID.
+  CHECK(all_tags.at(0) == std::pair<TagMap::id_t, TagMap::tag_t>(100, "#100"));
+  CHECK(all_tags.at(1) == std::pair<TagMap::id_t, TagMap::tag_t>(200, "#200"));
+  CHECK(all_tags.at(2) == std::pair<TagMap::id_t, TagMap::tag_t>(300, "#300"));
+  CHECK(all_tags.at(3) == std::pair<TagMap::id_t, TagMap::tag_t>(400, "#400"));
+  CHECK(all_tags.at(4) == std::pair<TagMap::id_t, TagMap::tag_t>(500, "#500"));
+
+  // Confirm that removing elements refreshes the results from getAllTags().
+  REQUIRE(map.removeTag(300));
+  REQUIRE(map.removeTag(500));
+  REQUIRE(map.removeTag(100));
+  all_tags = map.getAllTags();
+  REQUIRE(all_tags.size() == 2);
+  CHECK(all_tags.at(0) == std::pair<TagMap::id_t, TagMap::tag_t>(200, "#200"));
+  CHECK(all_tags.at(1) == std::pair<TagMap::id_t, TagMap::tag_t>(400, "#400"));
+}
