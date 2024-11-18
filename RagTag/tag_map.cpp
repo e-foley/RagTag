cec717f85b18d93fcac8ce3d7477e7b6646521dd
Ed Foley <ed.l.foley@gmail.com>
2024-11-18 15:38:10 -0800

    formatting: open brace on function def line

--- RagTag/tag_map.cpp
+++ RagTag/tag_map.cpp
@@ -5,8 +5,7 @@ const int TagMap::MAX_NUM_TAGS = std::numeric_limits<int>::max();
 
 TagMap::TagMap() {}
 
-bool TagMap::addTag(id_t id, tag_t tag)
-{
+bool TagMap::addTag(id_t id, tag_t tag) {
   if (numTags() >= MAX_NUM_TAGS) {
     return false;
   }
@@ -19,8 +18,7 @@ bool TagMap::removeTag(id_t id) {
   return id_to_tag_map_.erase(id) > 0;
 }
 
-std::optional<TagMap::tag_t> TagMap::getTag(id_t id) const
-{
+std::optional<TagMap::tag_t> TagMap::getTag(id_t id) const {
   const auto tag_it = id_to_tag_map_.find(id);
   if (tag_it == id_to_tag_map_.end()) {
     return {};
@@ -28,8 +26,7 @@ std::optional<TagMap::tag_t> TagMap::getTag(id_t id) const
   return tag_it->second;
 }
 
-std::vector<std::pair<TagMap::id_t, TagMap::tag_t>> TagMap::getAllTags() const
-{
+std::vector<std::pair<TagMap::id_t, TagMap::tag_t>> TagMap::getAllTags() const {
   std::vector<std::pair<id_t, tag_t>> tag_vector;
   tag_vector.reserve(id_to_tag_map_.size());
   for (const auto map_it : id_to_tag_map_) {
@@ -38,8 +35,7 @@ std::vector<std::pair<TagMap::id_t, TagMap::tag_t>> TagMap::getAllTags() const
   return tag_vector;
 }
 
-int TagMap::numTags() const
-{
+int TagMap::numTags() const {
   // Safe conversion provided MAX_NUM_TAGS is enforced.
   return static_cast<int>(id_to_tag_map_.size());
 }
