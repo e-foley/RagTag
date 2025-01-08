cfbc3140e01a0d7f2ad2969700ae5ee186a7863e
Ed Foley <ed.l.foley@gmail.com>
2025-01-08 14:59:17 -0800

    demo tag checkbox and edit button

--- RagTag/main_frame.cpp
+++ RagTag/main_frame.cpp
@@ -1,6 +1,6 @@
 #include "main_frame.h"
 
-MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Hello World") {
+MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "RagTag v0.0.1") {
   wxMenu* menuFile = new wxMenu;
   menuFile->Append(ID_HELLO, "&Hello...\tCtrl-H",
     "Help string shown in status bar for this menu item");
@@ -21,15 +21,31 @@ MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Hello World") {
 
 
   // Temporary: Build a panel as a proof of concept...
-  wxPanel* panel = new wxPanel(this, wxID_ANY);
-  wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
+  wxPanel* p_main = new wxPanel(this, wxID_ANY);
+  wxBoxSizer* sz_main = new wxBoxSizer(wxHORIZONTAL);
 
-  wxPanel* panel_l = new wxPanel(panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN);
-  wxPanel* panel_r = new wxPanel(panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN);
-  hbox->Add(panel_l, 1, wxEXPAND | wxALL, 5);
-  hbox->Add(panel_r, 1, wxEXPAND | wxALL, 5);
 
-  panel->SetSizer(hbox);
+  wxPanel* p_left = new wxPanel(p_main, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN);
+  wxBoxSizer* sz_left = new wxBoxSizer(wxVERTICAL);
+
+  wxPanel* p_tag_display = new wxPanel(p_left, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN);
+  wxBoxSizer* sz_tag_display = new wxBoxSizer(wxHORIZONTAL);
+
+  wxCheckBox* cb_tag_toggle = new wxCheckBox(p_tag_display, wxID_ANY, "Test");  // Second argument is checkbox ID.
+  sz_tag_display->Add(cb_tag_toggle, 1, wxEXPAND, 5);
+  wxButton* b_tag_edit = new wxButton(p_tag_display, wxID_ANY, "Edit");
+  sz_tag_display->Add(b_tag_edit, 1, wxEXPAND, 5);
+
+  p_tag_display->SetSizer(sz_tag_display);
+
+  sz_left->Add(p_tag_display, 1, wxEXPAND, 5);
+  p_left->SetSizer(sz_left);
+
+  wxPanel* p_right = new wxPanel(p_main, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN);
+  sz_main->Add(p_left, 1, wxEXPAND | wxALL, 5);
+  sz_main->Add(p_right, 1, wxEXPAND | wxALL, 5);
+
+  p_main->SetSizer(sz_main);
 
 
 
