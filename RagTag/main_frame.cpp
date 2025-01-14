c71d14c17fc38affd8b5bb351f856a05828b7004
Ed Foley <ed.l.foley@gmail.com>
2025-01-13 16:21:04 -0800

    first stab at new, open, save, save as ops
    Some features are not yet implemented—most significantly, error messages.

--- RagTag/main_frame.cpp
+++ RagTag/main_frame.cpp
@@ -1,5 +1,6 @@
 #include "main_frame.h"
 #include "tag_toggle_panel.h"
+#include <wx/filedlg.h>
 #include <wx/mediactrl.h>
 #include <wx/scrolwin.h>
 #include <wx/stdpaths.h>
@@ -80,16 +81,185 @@ MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "RagTag v0.0.1", wxDefaultPo
   sz_main->Add(p_left, 1, wxEXPAND | wxALL, 5);
   sz_main->Add(p_right, 1, wxEXPAND | wxALL, 5);
 
+  Bind(wxEVT_MENU, &MainFrame::OnNew, this, ID_NEW);
+  Bind(wxEVT_MENU, &MainFrame::OnOpen, this, ID_OPEN);
+  Bind(wxEVT_MENU, &MainFrame::OnSave, this, ID_SAVE);
+  Bind(wxEVT_MENU, &MainFrame::OnSaveAs, this, ID_SAVE_AS);
   Bind(wxEVT_MENU, &MainFrame::OnAbout, this, wxID_ABOUT);
   Bind(wxEVT_MENU, &MainFrame::OnExit, this, wxID_EXIT);
   Bind(wxEVT_MEDIA_LOADED, &MainFrame::OnMediaLoaded, this, ID_MEDIA_CTRL);
 }
 
+void MainFrame::OnNew(wxCommandEvent& event) {
+  if (is_dirty_) {
+    const UserIntention intention = promptUnsavedChanges();
+    switch (intention) {
+    case UserIntention::SAVE:
+      if (project_path_.has_value()) {
+        if (!saveProject()) {
+          // TODO: Report error.
+          return;
+        }
+      } else {
+        const std::optional<std::filesystem::path> path = promptSaveAs();
+        if (!path.has_value()) {
+          // User canceled dialog.
+          return;
+        }
+        if (!saveProjectAs(*path)) {
+          // Failed to save.
+          // TODO: Report error.
+          return;
+        }
+      }
+      break;
+    case UserIntention::DONT_SAVE:
+      break;
+    case UserIntention::CANCEL:
+      return;
+    default:
+      // TODO: Log error
+      return;
+    }
+  }
+
+  // If we've made it this far, we have permission to create a new project.
+  newProject();
+  SetStatusText("Created new project.");
+}
+
+void MainFrame::OnOpen(wxCommandEvent& event) {
+  if (is_dirty_) {
+    const UserIntention intention = promptUnsavedChanges();
+    switch (intention) {
+    case UserIntention::SAVE:
+      if (project_path_.has_value()) {
+        if (!saveProject()) {
+          // TODO: Report error.
+          return;
+        }
+      } else {
+        const std::optional<std::filesystem::path> path = promptSaveAs();
+        if (!path.has_value()) {
+          // User canceled dialog.
+          return;
+        }
+        if (!saveProjectAs(*path)) {
+          // Failed to save.
+          // TODO: Report error.
+          return;
+        }
+      }
+      break;
+    case UserIntention::DONT_SAVE:
+      break;
+    case UserIntention::CANCEL:
+      return;
+    default:
+      // TODO: Log error
+      return;
+    }
+  }
+
+  // If we've gotten this far, we have permission to open a file.
+  std::optional<std::filesystem::path> path_pending = promptOpen();
+  if (!path_pending.has_value()) {
+    // User canceled dialog.
+    return;
+  }
+  std::optional<ragtag::TagMap> tag_map_pending = ragtag::TagMap::fromFile(*path_pending);
+  if (!tag_map_pending.has_value()) {
+    // Loading operation failed.
+    // TODO: Report error
+    return;
+  }
+
+  // Loaded successfully!
+  tag_map_ = *tag_map_pending;
+  project_path_ = path_pending;
+  // TODO: Redraw dialog
+  is_dirty_ = false;
+  SetStatusText(L"Opened project " + project_path_->wstring() + L".");
+}
+
+void MainFrame::OnSave(wxCommandEvent& event) {
+  if (!project_path_.has_value()) {
+    const std::optional<std::filesystem::path> path = promptSaveAs();
+    if (!path.has_value()) {
+      // User canceled dialog.
+      return;
+    }
+    if (!saveProjectAs(*path)) {
+      // Failed to save.
+      // TODO: Report error.
+      return;
+    }
+    project_path_ = path;
+  } else if (!saveProject()) {
+    // TODO: Report error
+    return;
+  }
+
+  is_dirty_ = false;
+  SetStatusText(L"Saved project " + project_path_->wstring() + L".");
+}
+
+void MainFrame::OnSaveAs(wxCommandEvent& event) {
+  const std::optional<std::filesystem::path> path = promptSaveAs();
+  if (!path.has_value()) {
+    // User canceled dialog.
+    return;
+  }
+  if (!saveProjectAs(*path)) {
+    // Failed to save.
+    // TODO: Report error.
+    return;
+  }
+  project_path_ = path;
+  is_dirty_ = false;
+  SetStatusText(L"Saved project " + project_path_->wstring() + L".");
+}
+
 void MainFrame::OnExit(wxCommandEvent& event) {
+  if (is_dirty_) {
+    const UserIntention intention = promptUnsavedChanges();
+    switch (intention) {
+    case UserIntention::SAVE:
+      if (project_path_.has_value()) {
+        if (!saveProject()) {
+          // TODO: Report error.
+          return;
+        }
+      }
+      else {
+        const std::optional<std::filesystem::path> path = promptSaveAs();
+        if (!path.has_value()) {
+          // User canceled dialog.
+          return;
+        }
+        if (!saveProjectAs(*path)) {
+          // Failed to save.
+          // TODO: Report error.
+          return;
+        }
+      }
+      break;
+    case UserIntention::DONT_SAVE:
+      break;
+    case UserIntention::CANCEL:
+      return;
+    default:
+      // TODO: Log error
+      return;
+    }
+  }
+
+  // If we've gotten this far, we have permission to close.
   Close(true);
 }
 
 void MainFrame::OnAbout(wxCommandEvent& event) {
+  // (c) replacement technique from https://forums.wxwidgets.org/viewtopic.php?p=158583#p158583
   wxString about_string("RagTag v0.0.1\n\n(c) 2025 by Ed Foley");
 #if wxUSE_UNICODE
   const wxString copyright_symbol = wxString::FromUTF8("\xc2\xa9");
@@ -99,10 +269,66 @@ void MainFrame::OnAbout(wxCommandEvent& event) {
   wxMessageBox(about_string, "About", wxOK | wxICON_INFORMATION);
 }
 
-void MainFrame::OnHello(wxCommandEvent& event) {
-  wxLogMessage("Hello world from wxWidgets!");
-}
-
 void MainFrame::OnMediaLoaded(wxMediaEvent& event) {
   mc_media_display_->Play();
+}
+
+MainFrame::UserIntention MainFrame::promptUnsavedChanges() {
+  wxMessageDialog md_unsaved_changes(this, "You have unsaved changes.", "Unsaved Changes",
+      wxYES_NO | wxCANCEL | wxICON_WARNING);
+  md_unsaved_changes.SetYesNoCancelLabels("Save", "Don't Save", "Cancel");
+  const int selection = md_unsaved_changes.ShowModal();
+  switch (selection) {
+  case wxID_YES:
+    return UserIntention::SAVE;
+    break;
+  case wxID_NO:
+    return UserIntention::DONT_SAVE;
+    break;
+  default:
+  case wxID_CANCEL:
+    return UserIntention::CANCEL;
+  }
+}
+
+std::optional<std::filesystem::path> MainFrame::promptSaveAs() {
+  wxString wx_path = wxFileSelector("Save Project As", wxEmptyString, "project.tagdef", ".tagdef",
+    "RagTag project files (*.tagdef)|*.tagdef", wxFD_SAVE | wxFD_OVERWRITE_PROMPT, this);
+  if (wx_path.empty()) {
+    // User canceled the dialog.
+    return {};
+  }
+
+  return std::filesystem::path(wx_path.ToStdWstring());
+}
+
+std::optional<std::filesystem::path> MainFrame::promptOpen() {
+  wxString wx_path = wxFileSelector("Open Project", wxEmptyString, wxEmptyString, wxEmptyString,
+      "RagTag project files (*.tagdef)|*.tagdef", wxFD_OPEN | wxFD_FILE_MUST_EXIST, this);
+  if (wx_path.empty()) {
+    // User canceled the dialog.
+    return {};
+  }
+
+  return std::filesystem::path(wx_path.ToStdWstring());
+}
+
+void MainFrame::newProject() {
+  tag_map_ = ragtag::TagMap();
+  project_path_.reset();
+  is_dirty_ = true;
+}
+
+bool MainFrame::saveProject() {
+  if (!project_path_.has_value()) {
+    // This shouldn't happen per the function's preconditions.
+    // TODO: Log error
+    return false;
+  }
+
+  return tag_map_.toFile(*project_path_);
+}
+
+bool MainFrame::saveProjectAs(const std::filesystem::path& path) {
+  return tag_map_.toFile(path);
 }
--- RagTag/main_frame.h
+++ RagTag/main_frame.h
@@ -10,24 +10,42 @@
 class MainFrame : public wxFrame {
 public:
   enum {
-    ID_NEW = 1,
+    ID_NONE = 0,
+    ID_NEW,
     ID_OPEN,
     ID_SAVE,
     ID_SAVE_AS,
     ID_MEDIA_CTRL,
   };
 
+  enum class UserIntention {
+    NONE = 0,
+    SAVE,
+    DONT_SAVE,
+    CANCEL,
+  };
+
   MainFrame();
 
 private:
-  void OnHello(wxCommandEvent& event);
+  void OnNew(wxCommandEvent& event);
+  void OnOpen(wxCommandEvent& event);
+  void OnSave(wxCommandEvent& event);
+  void OnSaveAs(wxCommandEvent& event);
   void OnExit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
   void OnMediaLoaded(wxMediaEvent& event);
+  UserIntention promptUnsavedChanges();
+  std::optional<std::filesystem::path> promptSaveAs();
+  std::optional<std::filesystem::path> promptOpen();
+  void newProject();
+  bool saveProject();
+  bool saveProjectAs(const std::filesystem::path& path);
 
   wxMediaCtrl* mc_media_display_{ nullptr };
-  std::optional<ragtag::TagMap> tag_map_{};
-  std::filesystem::path project_path_{};
+  ragtag::TagMap tag_map_{};
+  std::optional<std::filesystem::path> project_path_{};
+  bool is_dirty_{ false };
 };
 
 #endif  // INCLUDE_MAIN_FRAME
