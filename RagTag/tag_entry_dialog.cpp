cd1a058f0fc00abf247356c5b3485f50631612e5
Ed Foley <ed.l.foley@gmail.com>
2025-01-20 15:47:27 -0800

    impl nonfunc tag entry dialog

--- RagTag/tag_entry_dialog.cpp
+++ RagTag/tag_entry_dialog.cpp
@@ -1,4 +1,8 @@
 #include "tag_entry_dialog.h"
+#include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/wx.h>
 
 TagEntryDialog::TagEntryDialog(wxWindow* parent)
   : TagEntryDialog(parent, std::optional<tag_entry_t>{}) {};
@@ -7,5 +11,76 @@ TagEntryDialog::TagEntryDialog(wxWindow* parent, const tag_entry_t& entry_init)
   : TagEntryDialog(parent, std::optional<tag_entry_t>{entry_init}) {}
 
 TagEntryDialog::TagEntryDialog(wxWindow* parent, const std::optional<tag_entry_t>& entry_init_opt)
-  : wxDialog(parent, wxID_ANY, "Create/Modify Tag", wxDefaultPosition, wxSize(400, 300)),
-  entry_(entry_init_opt) {}
+  : wxDialog(parent, wxID_ANY, "Create/Modify Tag", wxDefaultPosition, wxSize(300, 250)),
+  parent_(parent), entry_(entry_init_opt) {
+  wxBoxSizer* sz_rows = new wxBoxSizer(wxVERTICAL);
+  this->SetSizer(sz_rows);
+
+  wxBoxSizer* sz_tag_entry_row = new wxBoxSizer(wxHORIZONTAL);
+  wxStaticText* st_tag_name = new wxStaticText(this, wxID_ANY, "Tag name:");
+  sz_tag_entry_row->Add(st_tag_name, 0, wxALL, 5);
+  tc_tag_name_ = new wxTextCtrl(this, wxID_ANY, "tag");
+  sz_tag_entry_row->Add(tc_tag_name_, 1, wxALL, 5);
+  sz_rows->Add(sz_tag_entry_row, 0, wxEXPAND | wxALL, 5);
+
+  wxBoxSizer* sz_default_setting_row = new wxBoxSizer(wxHORIZONTAL);
+  //wxStaticText* st_default_setting_label = new wxStaticText(this, wxID_ANY, "Default setting:");
+  //sz_default_setting_row->Add(st_default_setting_label, 0, wxALL, 5);
+
+  // TODO: Tie this array closer to switch labels used in OnOk().
+  // NOTE: Make sure this and switch labels in OnOk() match!
+  wxString choices[3] = { "No", "Yes", "Uncommitted" };
+  rb_default_setting_ = new wxRadioBox(this, wxID_ANY, "Default state",
+    wxDefaultPosition, wxDefaultSize, 3, choices, 1, wxRA_SPECIFY_COLS);
+  sz_default_setting_row->Add(rb_default_setting_, 1, wxALL, 5);
+  sz_rows->Add(sz_default_setting_row, 0, wxEXPAND | wxALL, 5);
+
+  wxBoxSizer* sz_button_row = new wxBoxSizer(wxHORIZONTAL);
+  sz_button_row->AddStretchSpacer(1);
+  wxButton* b_cancel = new wxButton(this, ID_CANCEL, "Cancel", wxDefaultPosition, wxDefaultSize);
+  sz_button_row->Add(b_cancel, 0);
+  wxButton* b_ok = new wxButton(this, ID_OK, "OK", wxDefaultPosition, wxDefaultSize);
+  sz_button_row->Add(b_ok, 0);
+  sz_rows->Add(sz_button_row, 0, wxEXPAND | wxALL, 5);
+
+  Bind(wxEVT_BUTTON, &TagEntryDialog::OnOk, this, ID_OK);
+  Bind(wxEVT_BUTTON, &TagEntryDialog::OnCancel, this, ID_CANCEL);
+}
+
+std::optional<TagEntryDialog::tag_entry_t> TagEntryDialog::promptTagEntry() {
+  this->ShowModal();
+
+  return entry_;
+}
+
+void TagEntryDialog::OnOk(wxCommandEvent& event) {
+  tag_entry_t tag_entry_pending{};
+  tag_entry_pending.first = tc_tag_name_->GetLineText(1);
+
+  ragtag::TagProperties& tag_properties = tag_entry_pending.second;
+
+  // TODO: Tie these switch labels closer to the ordering of `choices` in the constructor.
+  // NOTE: Maintain same order/meaning as `choices` in constructor!
+  switch (rb_default_setting_->GetSelection()) {
+  case 0:
+    tag_properties.default_setting = ragtag::TagSetting::NO;
+    break;
+  case 1:
+    tag_properties.default_setting = ragtag::TagSetting::YES;
+    break;
+  default:
+  case 2:
+    tag_properties.default_setting = ragtag::TagSetting::UNCOMMITTED;
+    break;
+  }
+
+  entry_ = tag_entry_pending;
+
+  // TODO: Validate the entry first?
+  Close();
+}
+
+void TagEntryDialog::OnCancel(wxCommandEvent& event) {
+  entry_ = {};
+  Close();
+}
--- RagTag/tag_entry_dialog.h
+++ RagTag/tag_entry_dialog.h
@@ -3,17 +3,33 @@
 
 #include "tag_map.h"
 #include <wx/dialog.h>
+#include <wx/event.h>
+#include <wx/radiobox.h>
+#include <wx/textctrl.h>
+#include <wx/wx.h>
 
 class TagEntryDialog : public wxDialog {
 public:
   typedef std::pair<ragtag::tag_t, ragtag::TagProperties> tag_entry_t;
 
+  enum {
+    ID_CANCEL = 0,
+    ID_OK
+  };
+
   TagEntryDialog(wxWindow* parent);
   TagEntryDialog(wxWindow* parent, const tag_entry_t& entry_init);
+  std::optional<tag_entry_t> promptTagEntry();
 
 private:
   TagEntryDialog(wxWindow* parent, const std::optional<tag_entry_t>& entry_init_opt);
+  void OnOk(wxCommandEvent& event);
+  void OnCancel(wxCommandEvent& event);
+
   std::optional<std::pair<ragtag::tag_t, ragtag::TagProperties>> entry_{};
+  wxWindow* parent_{ nullptr };
+  wxTextCtrl* tc_tag_name_{ nullptr };
+  wxRadioBox* rb_default_setting_{ nullptr };
 };
 
 #endif  // INCLUDE_TAG_ENTRY_DIALOG
