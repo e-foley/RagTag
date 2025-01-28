31ca3738fa1df900d75efb4b1b6409364a101d5c
Ed Foley <ed.l.foley@gmail.com>
2025-01-28 15:18:09 -0800

    update most tests for new interface
    This gets the file read/write tests working, though I've disabled the file qualifier tests since the interface needs to be updated first.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -120,33 +120,39 @@ namespace ragtag {
     default_uncommitted.default_setting = TagSetting::UNCOMMITTED;
 
     // Registering tags...
-    tag_map_in.registerTag("Banana", default_no);
-    tag_map_in.registerTag("Cantaloupe", default_uncommitted);
-    tag_map_in.registerTag("Durian", default_no);
-    tag_map_in.registerTag("Apple", default_yes);
-    tag_map_in.deleteTag("Durian");  // Testing that deletion leaves no lingering artifacts
-    tag_map_in.registerTag("Dragonfruit");
+    CHECK(tag_map_in.registerTag("Banana", default_no));
+    CHECK(tag_map_in.registerTag("Cantaloupe", default_uncommitted));
+    CHECK(tag_map_in.registerTag("Durian", default_no));
+    CHECK(tag_map_in.registerTag("Apple", default_yes));
+    CHECK(tag_map_in.deleteTag("Durian"));  // Testing that deletion leaves no lingering artifacts
+    CHECK(tag_map_in.registerTag("Dragonfruit"));
 
     // Registering files...
-    tag_map_in.addFile("test1.madeup");
-    FileProperties test2_props;
-    test2_props.rating = 3.1f;
-    test2_props.tags.emplace("Dragonfruit", TagSetting::YES);
-    test2_props.tags.emplace("Cantaloupe", TagSetting::NO);
-    test2_props.tags.emplace("Banana", TagSetting::UNCOMMITTED);
-    tag_map_in.addFile("test2.madeup", test2_props);
-    FileProperties test3_props;
+    ragtag::path_t path = "test1.madeup";
+    CHECK(tag_map_in.addFile(path));
+
+    path = "test2.madeup";
+    CHECK(tag_map_in.addFile(path));
+    CHECK(tag_map_in.setRating(path, 3.1f));
+    CHECK(tag_map_in.setTag(path, "Dragonfruit", TagSetting::YES));
+    CHECK(tag_map_in.setTag(path, "Cantaloupe", TagSetting::NO));
+    CHECK(tag_map_in.setTag(path, "Banana", TagSetting::UNCOMMITTED));
+    // Leave "Apple" unspecified in test2.
+
+    path = "test3.madeup";
+    CHECK(tag_map_in.addFile(path));
     // Intentionally don't assign rating here.
-    test3_props.tags.emplace("Banana", TagSetting::NO);
-    test3_props.tags.emplace("Cantaloupe", TagSetting::YES);
-    test3_props.tags.emplace("Dragonfruit", TagSetting::NO);
-    test3_props.tags.emplace("Apple", TagSetting::YES);
-    tag_map_in.addFile("test3.madeup", test3_props);
-    FileProperties test4_props;
-    test4_props.rating = 1.9f;
-    test4_props.tags.emplace("Apple", TagSetting::NO);
-    tag_map_in.addFile("test4.madeup", test4_props);
-    tag_map_in.removeFile("test4.madeup");  // Testing that deletion leaves no lingering artifacts
+    CHECK(tag_map_in.setTag(path, "Banana", TagSetting::NO));
+    CHECK(tag_map_in.setTag(path, "Cantaloupe", TagSetting::YES));
+    CHECK(tag_map_in.setTag(path, "Dragonfruit", TagSetting::NO));
+    CHECK(tag_map_in.setTag(path, "Apple", TagSetting::YES));
+
+    path = "test4.madeup";
+    CHECK(tag_map_in.addFile(path));
+    CHECK(tag_map_in.setRating(path, 3.1f));
+    CHECK(tag_map_in.setTag(path, "Apple", TagSetting::NO));
+    // Testing that deletion leaves no lingering artifacts
+    CHECK(tag_map_in.removeFile("test4.madeup"));
     
     // Convert to JSON
     nlohmann::json j = tag_map_in.toJson();
@@ -156,15 +162,14 @@ namespace ragtag {
       REQUIRE(from_json_ret.has_value());
       CHECK(*from_json_ret == tag_map_in);
 
-      // Sanity check to make sure we didn't write/read empty file or something.
+      // Sanity checks to make sure we didn't write/read empty stream or something.
       CHECK(from_json_ret->numTags() == 4);
-      const auto test2_prop_ret = from_json_ret->getFileProperties("test2.madeup");
-      REQUIRE(test2_prop_ret.has_value());
-      REQUIRE(test2_prop_ret->rating.has_value());
-      CHECK(*test2_prop_ret->rating == 3.1f);
-      const auto tag_setting_it = test2_prop_ret->tags.find("Dragonfruit");
-      REQUIRE(tag_setting_it != test2_prop_ret->tags.end());
-      CHECK(tag_setting_it->second == TagSetting::YES);
+      const auto test2_rating_ret = from_json_ret->getRating("test2.madeup");
+      REQUIRE(test2_rating_ret.has_value());
+      CHECK(*test2_rating_ret == 3.1f);
+      const auto test2_tag_ret = from_json_ret->getTagSetting("test2.madeup", "Dragonfruit");
+      REQUIRE(test2_tag_ret.has_value());
+      CHECK(*test2_tag_ret == TagSetting::YES);
     }
 
     SECTION("File read/write") {
@@ -179,101 +184,100 @@ namespace ragtag {
       REQUIRE(constructed_from_file.has_value());
       CHECK(*constructed_from_file == tag_map_in);
 
-      // Sanity check to make sure we didn't write/read empty file or something.
+      // Sanity checks to make sure we didn't write/read empty file or something.
       CHECK(constructed_from_file->numTags() == 4);
-      const auto test3_prop_ret = constructed_from_file->getFileProperties("test3.madeup");
-      REQUIRE(test3_prop_ret.has_value());
-      REQUIRE_FALSE(test3_prop_ret->rating.has_value());
-      const auto tag_setting_it = test3_prop_ret->tags.find("Banana");
-      REQUIRE(tag_setting_it != test3_prop_ret->tags.end());
-      CHECK(tag_setting_it->second == TagSetting::NO);
+      const auto test3_rating_ret = constructed_from_file->getRating("test3.madeup");
+      CHECK_FALSE(test3_rating_ret.has_value());
+      const auto test3_tag_ret = constructed_from_file->getTagSetting("test3.madeup", "Banana");
+      REQUIRE(test3_tag_ret.has_value());
+      CHECK(*test3_tag_ret == TagSetting::NO);
 
       //std::remove("io.json");
     }
   }
 
-  TEST_CASE("TagMap selectFiles()", "[all][TagMap-6]") {
-    TagMap tag_map;
-    REQUIRE(tag_map.registerTag("2 legs"));
-    REQUIRE(tag_map.registerTag("4 legs"));
-    REQUIRE(tag_map.registerTag("6 legs"));
-    REQUIRE(tag_map.registerTag("flies"));
+  //TEST_CASE("TagMap selectFiles()", "[all][TagMap-6]") {
+  //  TagMap tag_map;
+  //  REQUIRE(tag_map.registerTag("2 legs"));
+  //  REQUIRE(tag_map.registerTag("4 legs"));
+  //  REQUIRE(tag_map.registerTag("6 legs"));
+  //  REQUIRE(tag_map.registerTag("flies"));
 
-    REQUIRE(tag_map.addFile("eagle"));
-    REQUIRE(tag_map.setTag("eagle", "2 legs", TagSetting::YES));
-    REQUIRE(tag_map.setTag("eagle", "4 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("eagle", "6 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("eagle", "flies", TagSetting::YES));
-    REQUIRE(tag_map.setRating("eagle", 4.5f));
+  //  REQUIRE(tag_map.addFile("eagle"));
+  //  REQUIRE(tag_map.setTag("eagle", "2 legs", TagSetting::YES));
+  //  REQUIRE(tag_map.setTag("eagle", "4 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("eagle", "6 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("eagle", "flies", TagSetting::YES));
+  //  REQUIRE(tag_map.setRating("eagle", 4.5f));
 
-    REQUIRE(tag_map.addFile("human"));
-    REQUIRE(tag_map.setTag("human", "2 legs", TagSetting::YES));
-    REQUIRE(tag_map.setTag("human", "4 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("human", "6 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("human", "flies", TagSetting::NO));
-    REQUIRE(tag_map.setRating("human", 4.0f));
+  //  REQUIRE(tag_map.addFile("human"));
+  //  REQUIRE(tag_map.setTag("human", "2 legs", TagSetting::YES));
+  //  REQUIRE(tag_map.setTag("human", "4 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("human", "6 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("human", "flies", TagSetting::NO));
+  //  REQUIRE(tag_map.setRating("human", 4.0f));
 
-    REQUIRE(tag_map.addFile("spider"));
-    REQUIRE(tag_map.setTag("spider", "2 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("spider", "4 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("spider", "6 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("spider", "flies", TagSetting::NO));
-    REQUIRE(tag_map.setRating("spider", 2.0f));
+  //  REQUIRE(tag_map.addFile("spider"));
+  //  REQUIRE(tag_map.setTag("spider", "2 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("spider", "4 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("spider", "6 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("spider", "flies", TagSetting::NO));
+  //  REQUIRE(tag_map.setRating("spider", 2.0f));
 
-    REQUIRE(tag_map.addFile("dog"));
-    REQUIRE(tag_map.setTag("dog", "2 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("dog", "4 legs", TagSetting::YES));
-    REQUIRE(tag_map.setTag("dog", "6 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("dog", "flies", TagSetting::NO));
-    REQUIRE(tag_map.setRating("dog", 5.0f));
+  //  REQUIRE(tag_map.addFile("dog"));
+  //  REQUIRE(tag_map.setTag("dog", "2 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("dog", "4 legs", TagSetting::YES));
+  //  REQUIRE(tag_map.setTag("dog", "6 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("dog", "flies", TagSetting::NO));
+  //  REQUIRE(tag_map.setRating("dog", 5.0f));
 
-    REQUIRE(tag_map.addFile("dragonfly"));
-    REQUIRE(tag_map.setTag("dragonfly", "2 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("dragonfly", "4 legs", TagSetting::NO));
-    REQUIRE(tag_map.setTag("dragonfly", "6 legs", TagSetting::YES));
-    REQUIRE(tag_map.setTag("dragonfly", "flies", TagSetting::YES));
-    REQUIRE(tag_map.setRating("dragonfly", 3.5f));
+  //  REQUIRE(tag_map.addFile("dragonfly"));
+  //  REQUIRE(tag_map.setTag("dragonfly", "2 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("dragonfly", "4 legs", TagSetting::NO));
+  //  REQUIRE(tag_map.setTag("dragonfly", "6 legs", TagSetting::YES));
+  //  REQUIRE(tag_map.setTag("dragonfly", "flies", TagSetting::YES));
+  //  REQUIRE(tag_map.setRating("dragonfly", 3.5f));
 
-    REQUIRE(tag_map.addFile("ditto"));
-    REQUIRE(tag_map.setTag("ditto", "2 legs", TagSetting::UNCOMMITTED));
-    REQUIRE(tag_map.setTag("ditto", "4 legs", TagSetting::UNCOMMITTED));
-    REQUIRE(tag_map.setTag("ditto", "6 legs", TagSetting::UNCOMMITTED));
-    REQUIRE(tag_map.setTag("ditto", "flies", TagSetting::UNCOMMITTED));
-    REQUIRE(tag_map.setRating("ditto", 123.4f));
+  //  REQUIRE(tag_map.addFile("ditto"));
+  //  REQUIRE(tag_map.setTag("ditto", "2 legs", TagSetting::UNCOMMITTED));
+  //  REQUIRE(tag_map.setTag("ditto", "4 legs", TagSetting::UNCOMMITTED));
+  //  REQUIRE(tag_map.setTag("ditto", "6 legs", TagSetting::UNCOMMITTED));
+  //  REQUIRE(tag_map.setTag("ditto", "flies", TagSetting::UNCOMMITTED));
+  //  REQUIRE(tag_map.setRating("ditto", 123.4f));
 
-    // Note: The predicates don't have to be assigned a variable, but doing so makes it easier to
-    // compose them into more complex predicates.
+  //  // Note: The predicates don't have to be assigned a variable, but doing so makes it easier to
+  //  // compose them into more complex predicates.
 
-    const TagMap::file_qualifier_t has_two_legs = [](const FileProperties& p) {
-      return p.tags.contains("2 legs") && p.tags.at("2 legs") == TagSetting::YES;
-    };
-    const auto two_legged_creatures = tag_map.selectFiles(has_two_legs);
-    CHECK(two_legged_creatures.size() == 2);
+  //  const TagMap::file_qualifier_t has_two_legs = [](const FileProperties& p) {
+  //    return p.tags.contains("2 legs") && p.tags.at("2 legs") == TagSetting::YES;
+  //  };
+  //  const auto two_legged_creatures = tag_map.selectFiles(has_two_legs);
+  //  CHECK(two_legged_creatures.size() == 2);
 
-    const TagMap::file_qualifier_t is_flightless = [](const FileProperties& p) {
-      return p.tags.contains("flies") && p.tags.at("flies") == TagSetting::NO;
-    };
-    const auto flightless_creatures = tag_map.selectFiles(is_flightless);
-    CHECK(flightless_creatures.size() == 3);
+  //  const TagMap::file_qualifier_t is_flightless = [](const FileProperties& p) {
+  //    return p.tags.contains("flies") && p.tags.at("flies") == TagSetting::NO;
+  //  };
+  //  const auto flightless_creatures = tag_map.selectFiles(is_flightless);
+  //  CHECK(flightless_creatures.size() == 3);
 
-    const TagMap::file_qualifier_t is_rated_at_least_four = [](const FileProperties& p) {
-      return p.rating >= 4.0f;
-    };
-    const auto creatures_rated_at_least_four = tag_map.selectFiles(is_rated_at_least_four);
-    CHECK(creatures_rated_at_least_four.size() == 4);
+  //  const TagMap::file_qualifier_t is_rated_at_least_four = [](const FileProperties& p) {
+  //    return p.rating >= 4.0f;
+  //  };
+  //  const auto creatures_rated_at_least_four = tag_map.selectFiles(is_rated_at_least_four);
+  //  CHECK(creatures_rated_at_least_four.size() == 4);
 
-    const TagMap::file_qualifier_t plausibly_has_four_legs = [](const FileProperties& p) {
-      return p.tags.contains("4 legs") &&
-        (p.tags.at("4 legs") == TagSetting::YES || p.tags.at("4 legs") == TagSetting::UNCOMMITTED);
-    };
-    const auto creatures_plausibly_with_four_legs = tag_map.selectFiles(plausibly_has_four_legs);
-    CHECK(creatures_plausibly_with_four_legs.size() == 2);
+  //  const TagMap::file_qualifier_t plausibly_has_four_legs = [](const FileProperties& p) {
+  //    return p.tags.contains("4 legs") &&
+  //      (p.tags.at("4 legs") == TagSetting::YES || p.tags.at("4 legs") == TagSetting::UNCOMMITTED);
+  //  };
+  //  const auto creatures_plausibly_with_four_legs = tag_map.selectFiles(plausibly_has_four_legs);
+  //  CHECK(creatures_plausibly_with_four_legs.size() == 2);
 
-    // Meta: There's probably an easier way than this to compose function predicates.
-    const auto flightless_or_high_rated_creatures =
-      tag_map.selectFiles([&is_flightless, &is_rated_at_least_four](const FileProperties& p) {
-        return is_flightless(p) || is_rated_at_least_four(p);
-      });
-    CHECK(flightless_or_high_rated_creatures.size() == 5);
-  }
+  //  // Meta: There's probably an easier way than this to compose function predicates.
+  //  const auto flightless_or_high_rated_creatures =
+  //    tag_map.selectFiles([&is_flightless, &is_rated_at_least_four](const FileProperties& p) {
+  //      return is_flightless(p) || is_rated_at_least_four(p);
+  //    });
+  //  CHECK(flightless_or_high_rated_creatures.size() == 5);
+  //}
 }  // namespace ragtag
