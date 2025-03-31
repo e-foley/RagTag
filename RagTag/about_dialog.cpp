2c7ca8b3536bcc446a746d5b95454dd052cf8ffe
Ed Foley <ed.l.foley@gmail.com>
2025-03-30 21:36:37 -0700

    about dialog progress

--- RagTag/about_dialog.cpp
+++ RagTag/about_dialog.cpp
@@ -14,12 +14,93 @@
 // <https://www.gnu.org/licenses/>.
 
 #include "about_dialog.h"
+#include "rag_tag_util.h"
 #include <wx/button.h>
+#include <wx/hyperlink.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
 
 AboutDialog::AboutDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, "About RagTag"),
     parent_(parent) {
+  SetSize(500, 700);
+  wxBoxSizer* sz_about = new wxBoxSizer(wxVERTICAL);
+  SetSizer(sz_about);
+  //sz_about->AddSpacer(20);
+
+  wxBoxSizer* sz_ragtag = new wxBoxSizer(wxVERTICAL);
+  // (c) replacement technique from https://forums.wxwidgets.org/viewtopic.php?p=158583#p158583
+  wxString about_string(L"RagTag " + RagTagUtil::getRagTagAppVersionString());
+  wxStaticText* st_ragtag = new wxStaticText(this, wxID_ANY, wxEmptyString, wxDefaultPosition,
+    wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
+  st_ragtag->SetLabelMarkup("<b>" + about_string + "</b>");
+  sz_ragtag->Add(st_ragtag, 0, wxALL | wxEXPAND, 2);
+  wxString copyright_string(L"Copyright (c) 2025 by Edward Foley");
+#if wxUSE_UNICODE
+  const wxString copyright_symbol = wxString::FromUTF8("\xc2\xa9");
+  copyright_string.Replace("(c)", copyright_symbol);
+#endif
+  sz_ragtag->Add(new wxStaticText(this, wxID_ANY, copyright_string, wxDefaultPosition,
+    wxDefaultSize, wxALIGN_CENTER_HORIZONTAL), 0, wxALL | wxEXPAND, 2);
+  sz_ragtag->Add(new wxStaticText(this, wxID_ANY,
+    "Released under the terms of the GNU General Public License v3.0",  wxDefaultPosition,
+    wxDefaultSize, wxALIGN_CENTER_HORIZONTAL), 0, wxALL | wxEXPAND, 2);
+  sz_ragtag->AddSpacer(10);
+  sz_ragtag->Add(new wxHyperlinkCtrl(this, wxID_ANY, "RagTag GitHub repository",
+    "https://github.com/e-foley/RagTag"), 0, wxALL | wxEXPAND, 2);
+  sz_ragtag->Add(new wxHyperlinkCtrl(this, wxID_ANY, "GNU General Public License v3.0",
+    "https://www.gnu.org/licenses/gpl-3.0.html"), 0, wxALL | wxEXPAND, 2);
+  sz_ragtag->Add(new wxHyperlinkCtrl(this, wxID_ANY, "RagTag logo designed by Freepik",
+    "https://www.freepik.com"), 0, wxALL | wxEXPAND, 2);
+  sz_ragtag->Add(new wxStaticText(this, wxID_ANY,
+    "This application was made possible thanks to other open-source projects:"),
+    0, wxALL | wxEXPAND, 2);
+  sz_about->Add(sz_ragtag, 0, wxEXPAND | wxALL, 10);
+  //sz_about->AddSpacer(20);
+
+  wxBoxSizer* sz_wxwidgets = new wxBoxSizer(wxVERTICAL);
+  wxStaticText* st_wxwidgets = new wxStaticText(this, wxID_ANY, wxEmptyString);
+  st_wxwidgets->SetLabelMarkup("<b>wxWidgets</b>");
+  sz_wxwidgets->Add(st_wxwidgets, 0, wxALL | wxEXPAND, 2);
+  sz_wxwidgets->Add(new wxHyperlinkCtrl(this, wxID_ANY, "wxWidgets website",
+    "https://wxwidgets.org/"), 0, wxALL | wxEXPAND, 2);
+  sz_wxwidgets->Add(new wxHyperlinkCtrl(this, wxID_ANY, "wxWidgets GitHub repository",
+    "https://github.com/wxWidgets/wxWidgets"), 0, wxALL | wxEXPAND, 2);
+  sz_wxwidgets->Add(new wxHyperlinkCtrl(this, wxID_ANY, "wxWindows Library Licence v3.1",
+    "https://wxwidgets.org/about/licence/"), 0, wxALL | wxEXPAND, 2);
+  sz_about->Add(sz_wxwidgets, 0, wxEXPAND | wxALL, 10);
+  //sz_about->AddSpacer(20);
+
+  wxBoxSizer* sz_json = new wxBoxSizer(wxVERTICAL);
+  wxStaticText* st_json = new wxStaticText(this, wxID_ANY, wxEmptyString);
+  st_json->SetLabelMarkup("<b>JSON for Modern C++ by Niels Lohmann</b>");
+  sz_json->Add(st_json, 0, wxALL | wxEXPAND, 2);
+  sz_json->Add(new wxHyperlinkCtrl(this, wxID_ANY, "JSON for Modern C++ website",
+    "https://json.nlohmann.me/"), 0, wxALL | wxEXPAND, 2);
+  sz_json->Add(new wxHyperlinkCtrl(this, wxID_ANY, "JSON for Modern C++ GitHub repository",
+    "https://github.com/nlohmann/json/"), 0, wxALL | wxEXPAND, 2);
+  sz_json->Add(new wxHyperlinkCtrl(this, wxID_ANY, "MIT License",
+    "https://github.com/nlohmann/json/blob/develop/LICENSE.MIT"), 0, wxALL | wxEXPAND, 2);
+  sz_about->Add(sz_json, 0, wxEXPAND | wxALL, 10);
+  //sz_about->AddSpacer(20);
+
+  wxBoxSizer* sz_catch = new wxBoxSizer(wxVERTICAL);
+  wxStaticText* st_catch = new wxStaticText(this, wxID_ANY, wxEmptyString);
+  st_catch->SetLabelMarkup("<b>Catch2 by </b>");
+  sz_catch->Add(st_catch, 0, wxALL | wxEXPAND, 2);
+  sz_catch->Add(new wxHyperlinkCtrl(this, wxID_ANY, "Catch2 GitHub repository",
+    "https://github.com/catchorg/Catch2"), 0, wxALL | wxEXPAND, 2);
+  sz_catch->Add(new wxHyperlinkCtrl(this, wxID_ANY, "Boost Software License v1.0",
+    "https://github.com/catchorg/Catch2/blob/devel/LICENSE.txt"), 0, wxALL | wxEXPAND, 2);
+  sz_about->Add(sz_catch, 0, wxEXPAND | wxALL, 10);
+
+  //wxBoxSizer* sz_freepik = new wxBoxSizer(wxVERTICAL);
+  //wxStaticText* st_freepik = new wxStaticText(this, wxID_ANY, wxEmptyString);
+  //st_freepik->SetLabelMarkup("<b>Freepik</b>");
+  //sz_freepik->Add(st_freepik, 0, wxALL | wxEXPAND, 2);
+
+  //sz_about->Add(sz_freepik, 0, wxEXPAND | wxALL, 10);
+
+  //SetIcons(wxIconBundle(wxICON(IDI_ICON1)));
   //response_.tag = tag;
   //response_.tag_properties = tag_properties;
 
